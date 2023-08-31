5b263e599f016e31488bf7617daaaf41fe6130aa
Ben Duncan <benduncan@Bens-MacBook-Air-72.local>
2023-08-31 17:01:21 -0600

    Updated init llama.bash.

--- init_llama.bash
+++ init_llama.bash
@@ -3,12 +3,29 @@
 # Created for the BYU ECEn Shop
 # Created on Aug 3, 2023
 
-printf "\n\ncreating llama.cpp folder"
-mkdir llama.cpp
-cd llama.cpp
-
-printf "\n\ncloning llama.cpp project"
+printf ">>> \n\ncloning llama.cpp project"
 git clone https://github.com/ggerganov/llama.cpp.git
-printf "done cloning llama.cpp project"
+cd llama.cpp
+printf ">>> done cloning llama.cpp project"
 
-printf "\n\n... there needs to be more to this script. download the models with the download model script."
+# printf ">>> \n\n... there needs to be more to this script. download the models with the download model script."
+
+printf ">>> making llama.cppp"
+make
+
+printf ">>> downloading models using the downloadmodels.bash script"
+cd ../
+bash downloadmodels.bash
+
+printf ">>> finished downloading models"
+
+cd llama.cpp/models/
+
+printf ">>> installing python dependencies"
+python3 -m pip install -r requirements.txt || true
+
+printf ">>> converting models"
+python3 convert.py models/7B/
+
+printf ">>> quantizing models"
+./quantize ./models/7B/ggml-model-f16.gguf ./models/7B/ggml-model-q4_0.gguf q4_0
